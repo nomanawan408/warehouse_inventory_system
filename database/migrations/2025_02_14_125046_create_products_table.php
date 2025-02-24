ae3933f3dec3d724c9f4ed25c985d442bf3ad513
Noman AWan <awanzada408@gmail.com>
2025-02-24 13:00:14 +0500

    adding new updates

--- database/migrations/2025_02_14_125046_create_products_table.php
+++ database/migrations/2025_02_14_125046_create_products_table.php
@@ -11,25 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->decimal('purchase_price', 10, 2);
-            $table->decimal('sale_price', 10, 2);
-            $table->integer('quantity');
-            $table->boolean('status');
-            $table->timestamps();
-        });
-
         // Schema::create('products', function (Blueprint $table) {
         //     $table->id();
         //     $table->string('name');
         //     $table->decimal('purchase_price', 10, 2);
         //     $table->decimal('sale_price', 10, 2);
-        //     $table->integer('stock_quantity');  // Renamed from 'quantity'
-        //     $table->boolean('status')->default(true);
+        //     $table->integer('quantity');
+        //     $table->boolean('status');
         //     $table->timestamps();
         // });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('purchase_price', 10, 2);
+            $table->decimal('sale_price', 10, 2);
+            $table->integer('stock_quantity');  // Renamed from 'quantity'
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
     }
 
     /**
